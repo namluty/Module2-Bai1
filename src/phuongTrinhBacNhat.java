de03ca7e0d4c949f5e62ee3ef9bd5701d7562e09
DO HOAI NAM <dhnam.2020@gmail.com>
2021-07-05 15:50:44 +0700

    phuong trinh bac nhat

---
+++ src/phuongTrinhBacNhat.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class phuongTrinhBacNhat {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c', please enter constants:");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("a: ");
+        double a = scan.nextDouble();
+        System.out.print("b: ");
+        double b = scan.nextDouble();
+        System.out.print("c: ");
+        double c = scan.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Phuong tirnh co nghiem x = %.3f", answer);
+        } else {
+            if (b == c) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
